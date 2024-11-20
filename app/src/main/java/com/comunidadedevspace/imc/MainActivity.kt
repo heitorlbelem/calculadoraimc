24c4a889024e5488ec778283e415a105c50c0861
Heitor Belém <heitorbelem@Heitors-MacBook-Pro.local>
2024-11-20 20:18:31 -0300

    Cria componentes para interação com usuário

--- app/src/main/java/com/comunidadedevspace/imc/MainActivity.kt
+++ app/src/main/java/com/comunidadedevspace/imc/MainActivity.kt
@@ -2,10 +2,35 @@ package com.comunidadedevspace.imc
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import com.google.android.material.snackbar.Snackbar
+import com.google.android.material.textfield.TextInputEditText
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val edtWeight = findViewById<TextInputEditText>(R.id.edt_weight)
+        val edtHeight = findViewById<TextInputEditText>(R.id.edt_height)
+        val btnCalculate = findViewById<Button>(R.id.btn_calculate)
+
+        btnCalculate.setOnClickListener {
+            val weightStr: String = edtWeight.text.toString()
+            val heightStr: String = edtHeight.text.toString()
+            if(weightStr.isEmpty() || heightStr.isEmpty()) {
+                Snackbar.make(
+                    edtHeight,
+                    "Preencha todos os campos",
+                    Snackbar.LENGTH_LONG
+                ).show()
+            } else {
+                val weight = weightStr.toFloat()
+                val height = heightStr.toFloat()
+                val imc = weight / (height * height)
+                println("IMC: " + imc)
+            }
+
+        }
     }
 }
