d3fc33cba37f7e34cc0444f770984baf5608d14d
Heitor Belém <heitorbelem@192.168.0.6>
2024-11-21 02:40:45 -0300

    Finaliza app de IMC

--- app/src/main/java/com/comunidadedevspace/imc/MainActivity.kt
+++ app/src/main/java/com/comunidadedevspace/imc/MainActivity.kt
@@ -1,10 +1,13 @@
 package com.comunidadedevspace.imc
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import com.google.android.material.snackbar.Snackbar
 import com.google.android.material.textfield.TextInputEditText
+
+const val KEY_IMC_RESULT = "ResultActivity.KEY_IMC"
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -28,7 +31,9 @@ class MainActivity : AppCompatActivity() {
                 val weight = weightStr.toFloat()
                 val height = heightStr.toFloat()
                 val imc = weight / (height * height)
-                println("IMC: " + imc)
+                val intent = Intent(this, ResultActivity::class.java)
+                intent.putExtra(KEY_IMC_RESULT, imc)
+                startActivity(intent)
             }
 
         }
---
+++ app/src/main/java/com/comunidadedevspace/imc/ResultActivity.kt
@@ -0,0 +1,44 @@
+package com.comunidadedevspace.imc
+
+import android.graphics.Color
+import android.os.Bundle
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
+
+class ResultActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_result)
+
+        val imc = intent.getFloatExtra(KEY_IMC_RESULT, 0f)
+        val tvResult = findViewById<TextView>(R.id.tv_result)
+        val tvClassification = findViewById<TextView>(R.id.tv_classification)
+
+        var classification: String? = null
+        var classificationColor: Int? = null
+
+        if(imc <= 18.5f) {
+            classification = "MAGREZA"
+            classificationColor = R.color.sky_blue
+        } else if (imc <= 24.9f) {
+            classification = "NORMAL"
+            classificationColor = R.color.lime_green
+        } else if (imc <= 29.9f) {
+            classification = "SOBREPESO"
+            classificationColor = R.color.gold
+        } else if (imc <= 39.9f) {
+            classification = "OBESIDADE"
+            classificationColor = R.color.orange
+        } else {
+            classification = "OBESIDADE GRAVE"
+            classificationColor = R.color.red
+        }
+
+        tvResult.text = imc.toString()
+        tvClassification.text = classification
+        tvClassification.setTextColor(ContextCompat.getColor(this, classificationColor))
+    }
+}
